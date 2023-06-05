96c3731a661f5b37ce92dc82b96b031cc1c91897
pofagyalu <pofagyalu@gmail.com>
2023-06-05 14:16:52 +0200

    added job controller, model and routes

---
+++ models/Job.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    link: {
+      type: String,
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enumb: ['interview', 'declinded', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Job', jobSchema);
