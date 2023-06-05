7dfd57b660d85adc023fcad08f086af41684201b
pofagyalu <pofagyalu@gmail.com>
2023-06-05 15:14:01 +0200

    swagger docs added

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 require('express-async-errors');
+
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -10,12 +20,25 @@ const authenticateUser = require('./middleware/authentication');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
-// error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get('/', (req, res) => {
+  res.send('<h1> Somer API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
